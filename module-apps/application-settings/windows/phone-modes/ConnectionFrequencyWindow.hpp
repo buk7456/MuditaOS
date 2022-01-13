1080e7955e29fb680769bb78a57525838100c0a4
Pawel Olejniczak <pawel.olejniczak@mudita.com>
2022-01-13 19:08:35 +0100

    [EGD-7359] Remove 0 connection frequency for message only mode
    When Offline/Message only mode is set, Pure should log into
    the network to send and download messages based on
    connection frequency. The default option was set to 0, which
    meant that message only mode was not working at all.

--- module-apps/application-settings/windows/phone-modes/ConnectionFrequencyWindow.hpp
+++ module-apps/application-settings/windows/phone-modes/ConnectionFrequencyWindow.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2021, Mudita Sp. z.o.o. All rights reserved.
+// Copyright (c) 2017-2022, Mudita Sp. z.o.o. All rights reserved.
 // For licensing, see https://github.com/mudita/MuditaOS/LICENSE.md
 
 #pragma once
@@ -19,7 +19,7 @@ namespace gui
         void rebuild() override;
         app::settingsInterface::ConnectionSettings *connectionSettings;
         void updateInterval(uint8_t value);
-        std::vector<uint8_t> frequency{0, 15, 30, 45, 60};
+        std::vector<uint8_t> frequency{15, 30, 45, 60};
 
       public:
         ConnectionFrequencyWindow(app::ApplicationCommon *app,
